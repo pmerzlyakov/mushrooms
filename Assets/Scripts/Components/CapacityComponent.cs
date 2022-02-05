e7590e99446cac2df802a420791ac44ef28b56e0
bimmy <kolgatinadasha@gmail.com>
2022-02-05 22:35:34 +0300

    trash commit

---
+++ Assets/Scripts/Components/CapacityComponent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mushrooms
+{
+    public struct CapacityComponent
+    {
+        public GameObject murshroom;
+    }
+}
---
+++ Assets/Scripts/Systems/SpawnSystem.cs
@@ -0,0 +1,69 @@
+using Leopotam.EcsLite;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mushrooms
+{
+    public class SpawnSystem : IEcsRunSystem
+    {
+        // private EcsFilter<HitComponent> shoots;
+        // private EcsFilter<PlayerComponent> playerFilter;
+
+        // public void Run () {
+        //     // Will be called on each EcsSystems.Run() call.
+        // }
+
+        // void IEcsRunSystem.Run()
+        // {
+        //           // test creating and removing entites in loop.
+        //     var entity = _world.CreateEntity ();
+        //     _world.AddComponent<UnityIntegrationComponent1> (entity);
+        //     _world.RemoveEntity (entity);
+
+        //     // change component field in loop.
+        //     foreach (var c2entity in _filter.Entities) {
+        //         _world.GetComponent<UnityIntegrationComponent2> (c2entity).Time = Time.time;
+        //     }
+
+        //     foreach (var i in shoots)
+        //     {
+        //         ref var hitComponent = ref shoots.Get1(i);
+
+        //         foreach(var j in playerFilter)
+        //         {
+        //             ref var playerComponent = ref playerFilter.Get1(i);
+
+        //             if (hitComponent.other.CompareTag("WinPoint"))
+        //             {
+        //                 playerComponent.playerTransform.gameObject.SetActive(false);
+        //                 playerFilter.GetEntity(i).Destroy();
+        //                 gameData.playerWonPanel.SetActive(true);
+        //             }
+
+        //             if (hitComponent.other.CompareTag("JumpBuff"))
+        //             {
+        //                 hitComponent.other.gameObject.SetActive(false);
+        //                 playerComponent.playerJumpForce *= 2f;
+        //                 ref var jumpBuffComponent = ref playerFilter.GetEntity(i).Get<JumpBuffComponent>();
+        //                 jumpBuffComponent.timer = gameData.configuration.jumpBuffDuration;
+        //             }
+
+        //             if (hitComponent.other.CompareTag("SpeedBuff"))
+        //             {
+        //                 hitComponent.other.gameObject.SetActive(false);
+        //                 playerComponent.playerSpeed *= 2f;
+        //                 ref var speedBuffComponent = ref playerFilter.GetEntity(i).Get<SpeedBuffComponent>();
+        //                 speedBuffComponent.timer = gameData.configuration.speedBuffDuration;
+        //             }
+        //         }
+                
+        //     }
+        // }
+        public void Run(EcsSystems systems)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
